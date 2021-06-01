e03ecb6e74742d3ca577cd92191fa8f33d381909
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2021-06-01 13:30:42 +0300

    add it into bin instead of sbin

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ echo Marking iptrace.py as executable...
 chmod +x iptrace.py
 
 echo Creating symlink
-ln -s $(pwd)/iptrace.py /sbin/iptrace
+ln -s $(pwd)/iptrace.py /bin/iptrace
 
 echo Everything\'s set up! Run iptrace to get started.
